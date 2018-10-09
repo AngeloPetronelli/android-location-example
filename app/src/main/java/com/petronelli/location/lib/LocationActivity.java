7516fd00a4a2acf0bff4045b1bede2678625dc9a
Angelo <angelo.petronelli@gmail.com>
2018-10-09 23:04:35 +0200

    fused location provider

--- app/src/main/java/com/petronelli/location/lib/LocationActivity.java
+++ app/src/main/java/com/petronelli/location/lib/LocationActivity.java
@@ -4,85 +4,78 @@ import android.Manifest;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Bundle;
+import android.os.Looper;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.SettingsClient;
 
 /**
  * @author Angelo Petronelli on 23/05/2017.
  */
-public class LocationActivity extends AppCompatActivity implements LocationListener,
-        GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
-    private final String TAG = getClass().getSimpleName();
+public abstract class LocationActivity extends AppCompatActivity {
+
+    private final String TAG = "LocationActivity";
 
     private static final int PLAY_SERVICES_RESOLUTION_REQUEST = 100;
     private static final int PERMISSION_RESOLUTION_REQUEST = 101;
 
-    private static final int FAST_INTERVAL = 6000;     // 6 second
-    private static final int INTERVAL = 60000;         // 1 minute
+    private static final long UPDATE_INTERVAL_IN_MILLISECONDS = 10000;
+    private static final long FASTEST_UPDATE_INTERVAL_IN_MILLISECONDS = UPDATE_INTERVAL_IN_MILLISECONDS / 2;
 
-    private GoogleApiClient mGoogleApiClient;
-    private LocationRequest mLocationRequest;
+    private FusedLocationProviderClient providerClient;
+    private SettingsClient settingsClient;
+    private LocationSettingsRequest locationSettingsRequest;
 
-    private LocationCallback callback;
+    private LocationCallback locationCallback;
 
-    public void setLocationCallback(LocationCallback callback) {
-        this.callback = callback;
-    }
+    private LocationRequest locationRequest;
+    private Location currentLocation;
+
+    public abstract void onLocationChanged(Location location);
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        if (isGooglePlayServicesAvailable(false)) {
-            mGoogleApiClient = new GoogleApiClient.Builder(this)
-                    .addApi(LocationServices.API)
-                    .addConnectionCallbacks(this)
-                    .addOnConnectionFailedListener(this)
-                    .build();
+        if (isGooglePlayServicesAvailable(true)) {
+            providerClient = LocationServices.getFusedLocationProviderClient(this);
+            settingsClient = LocationServices.getSettingsClient(this);
 
-            mLocationRequest = new LocationRequest();
-            mLocationRequest.setFastestInterval(FAST_INTERVAL);
-            mLocationRequest.setInterval(INTERVAL);
-            mLocationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+            locationRequest = LocationRequest.create();
+            locationRequest.setInterval(UPDATE_INTERVAL_IN_MILLISECONDS);
+            locationRequest.setFastestInterval(FASTEST_UPDATE_INTERVAL_IN_MILLISECONDS);
+            locationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+
+            locationCallback = new LocationCallback() {
+                @Override
+                public void onLocationResult(LocationResult locationResult) {
+                    super.onLocationResult(locationResult);
+
+                    if (currentLocation != null) {
+                        currentLocation = locationResult.getLastLocation();
+
+                        onLocationChanged(currentLocation);
+                    }
+                }
+            };
+
+            LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder();
+            builder.addLocationRequest(locationRequest);
+            locationSettingsRequest = builder.build();
         }
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-        if (mGoogleApiClient != null) {
-            mGoogleApiClient.connect();
-        }
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-        if (mGoogleApiClient != null) {
-            mGoogleApiClient.disconnect();
-        }
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        startLocationUpdates();
-    }
-
-    @Override
-    public void onPause() {
-        super.onPause();
-        stopLocationUpdates();
     }
 
     @Override
@@ -91,31 +84,7 @@ public class LocationActivity extends AppCompatActivity implements LocationListe
 
         if (requestCode == PERMISSION_RESOLUTION_REQUEST &&
                 (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED)) {
-            startLocationUpdates();
-        }
-    }
-
-    public void startLocationUpdates() {
-        if (mGoogleApiClient != null && mGoogleApiClient.isConnected()) {
-            if (checkLocationPermission()) {
-                LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, mLocationRequest, this);
-            }
-        }
-    }
-
-    public void stopLocationUpdates() {
-        if (mGoogleApiClient != null && mGoogleApiClient.isConnected()) {
-            if (checkLocationPermission()) {
-                LocationServices.FusedLocationApi.removeLocationUpdates(mGoogleApiClient, this);
-            }
-        }
-    }
-
-    public void getLastLocation() {
-        if (mGoogleApiClient != null && mGoogleApiClient.isConnected()) {
-            if (checkLocationPermission()) {
-                onLocationChanged(LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient));
-            }
+            startLocationUpdated();
         }
     }
 
@@ -139,33 +108,22 @@ public class LocationActivity extends AppCompatActivity implements LocationListe
         return true;
     }
 
-    ////
+    //// START/STOP
 
-    @Override
-    public void onConnected(Bundle bundle) {
-        if (checkLocationPermission()) {
-            startLocationUpdates();
-        }
+    public void startLocationUpdated() {
+        settingsClient.checkLocationSettings(locationSettingsRequest)
+                .addOnSuccessListener(this, locationSettingsResponse -> {
+                    if (checkLocationPermission()) {
+                        Log.i(TAG, "startLocationUpdated");
+                        providerClient.requestLocationUpdates(locationRequest, locationCallback, Looper.myLooper());
+                    }
+                })
+                .addOnFailureListener(this, e -> Log.e(TAG, e.getLocalizedMessage()));
     }
 
-    @Override
-    public final void onLocationChanged(Location location) {
-        if (callback != null) {
-            callback.locationChanged(location);
-        }
+    public void stopLocationUpdated() {
+        providerClient.removeLocationUpdates(locationCallback)
+                .addOnCompleteListener(this, task -> Log.i(TAG, "stopLocationUpdated"));
     }
 
-    @Override
-    public final void onConnectionSuspended(int i) {
-        if (callback != null) {
-            callback.connectionSuspended(i);
-        }
-    }
-
-    @Override
-    public final void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
-        if (callback != null) {
-            callback.connectionFailed(connectionResult);
-        }
-    }
 }
